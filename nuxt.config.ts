0b009a323e9e716fb1814b5b8085357004f48b00
Luis Sena <luis.senarode@gmail.com>
2023-11-16 17:39:33 -0300

    feat: title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Luis Sena",
+    },
+  },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   modules: ["nuxt-icon"],
